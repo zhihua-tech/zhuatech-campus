6d3337aefdb7e30dac48f7de3eebb8c6789ea6d9
nero <233401861@qq.com>
2026-08-22 10:31:18 +0800

    chore: add website to copyright headers

--- backend/src/main/java/cn/zhuatech/campus/controller/CrowdPressureController.java
+++ backend/src/main/java/cn/zhuatech/campus/controller/CrowdPressureController.java
@@ -1,4 +1,4 @@
-/* Copyright 2026 上海如静知华信息科技有限公司 */
+/* Copyright 2026 上海如静知华信息科技有限公司 · https://www.zhuatech.cn/ */
 package cn.zhuatech.campus.controller;
 import cn.zhuatech.campus.common.ApiResponse;import cn.zhuatech.campus.service.CrowdPressureService;import jakarta.validation.Valid;import org.springframework.web.bind.annotation.*;
 @RestController @RequestMapping("/api/campus/insights/crowd-pressure") public class CrowdPressureController {private final CrowdPressureService service;public CrowdPressureController(CrowdPressureService service){this.service=service;}@PostMapping ApiResponse<CrowdPressureService.Result> forecast(@Valid @RequestBody CrowdPressureService.Request request){return ApiResponse.ok(service.forecast(request));}}
